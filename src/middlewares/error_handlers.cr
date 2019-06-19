33c625cf1f5166d3b7d67e7b03ce8f93caf9d4db
D. Scott Boggs <scott@tams.tech>
2019-06-19 09:53:46 -0400

    Only handle error statuses, as opposed to all statuses

--- src/middlewares/error_handlers.cr
+++ src/middlewares/error_handlers.cr
@@ -1,10 +1,14 @@
 macro initialize_error_handlers
   {% for code in HTTP::Status.constants %}
-  Kemal.config.add_error_handler HTTP::Status::{{code.id}}.value do |context, exception|
-    # TODO: a better logging solution
-    log "ERROR@#{Time.utc}: #{exception.message}"
-    {errors: [exception.message]}.to_json context.response
-    context.response.flush
+  if HTTP::Status::{{code}}.value >= 400
+    Kemal.config.add_error_handler HTTP::Status::{{code.id}}.value do |context, exception|
+      # TODO: a better logging solution
+      log "ERROR@#{Time.utc}: #{exception.message}"
+      context.response.status_code = exception.status_code.value if exception.responds_to? :status_code
+      {errors: [exception.message]}.to_json context.response
+      context.response.flush
+      nil
+    end
   end
   {% end %}
 end
